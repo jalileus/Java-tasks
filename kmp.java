933e4656995c1a34ee3cb8e8224728a368a33da9
Jalal Alhajali <alhajalijalal@gmail.com>
2025-02-08 19:33:42 +0300

    Update kmp.java

--- kmp.java
+++ kmp.java
@@ -2,62 +2,58 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class kmp {
-    public static int[] getLpc(String substring, int subStringSize){
-    int[] lps = new int[subStringSize];
-    int len = 0;
-    lps[0] = 0;
-    int i = 1;
+public class KMP {
 
-    while(i < subStringSize) {
-        if(substring.charAt(i) == substring.charAt(len)) {
-            len++;
-            lps[i] = len;
-            i++;
-        }
-        else {
-            if(len != 0){
-                len = lps[len - 1];
-            }
-            else {
-                lps[i] = 0;
+    public static int[] getLpc(String substring, int subStringSize) {
+        int[] lps = new int[subStringSize];
+        int len = 0;
+        lps[0] = 0;
+        int i = 1;
+
+        while (i < subStringSize) {
+            if (substring.charAt(i) == substring.charAt(len)) {
+                len++;
+                lps[i] = len;
                 i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
             }
         }
+        return lps;
     }
-    return lps;
-}
 
-public static void  kmpSearch(String subString, String text) {
+    public static void kmpSearch(String subString, String text) {
+        int subStringSize = subString.length();
+        int textSize = text.length();
 
-    int subStringSize = subString.length();
-    int textSize = text.length();
+        int[] lps = getLpc(subString, subStringSize);
 
+        int i = 0;
+        int j = 0;
 
-    int[] lps = getLpc(subString, subStringSize);
-
-    int i = 0;
-    int j = 0;
-
-    while ((textSize - i) >= (subStringSize - j)) {
-        if(subString.charAt(j) == text.charAt((i))) {
-            i++;
-            j++;
-        } 
-        if(j == subStringSize) {
-            System.out.print(i - j + " ");
-            j = lps[j - 1];
-        }
-        else if (i < textSize && subString.charAt(j) != text.charAt(i)) {
-            if (j != 0){
+        while ((textSize - i) >= (subStringSize - j)) {
+            if (subString.charAt(j) == text.charAt(i)) {
+                i++;
+                j++;
+            }
+            if (j == subStringSize) {
+                System.out.print((i - j) + " ");
                 j = lps[j - 1];
-            }
-            else {
-                i = i + 1;
+            } else if (i < textSize && subString.charAt(j) != text.charAt(i)) {
+                if (j != 0) {
+                    j = lps[j - 1];
+                } else {
+                    i = i + 1;
+                }
             }
         }
     }
-}
+
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String subString = reader.readLine();
@@ -66,6 +62,3 @@ public static void  kmpSearch(String subString, String text) {
         reader.close();
     }
 }
-
-
-
